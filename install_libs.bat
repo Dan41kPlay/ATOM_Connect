2c5ad4cb24affb450084e59cff1803ea03e68cec
Dan41kPlay <86301060+Dan41kPlay@users.noreply.github.com>
2023-10-12 12:39:24 +0300

    Update install_libs.bat

--- install_libs.bat
+++ install_libs.bat
@@ -5,4 +5,7 @@ pip3.11 install fuzzywuzzy
 pip3.11 install ujson
 pip3.11 install sounddevice
 pip3.11 install vosk
+pip3.11 install omegaconf
+pip3.11 install numpy
+pip3.11 install python-Levenshtein
 pip3.11 install -e git+https://github.com/Yuego/num2t4ru#egg=num2t4ru
