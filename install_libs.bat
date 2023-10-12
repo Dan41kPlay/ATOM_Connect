863943343caf2edf95891c6e85fe3f4e4579739d
Dan41kPlay <86301060+Dan41kPlay@users.noreply.github.com>
2023-10-12 12:20:08 +0300

    add installation and launch files

---
+++ install_libs.bat
@@ -0,0 +1,8 @@
+@echo off
+pip3.11 install torch
+pip3.11 install pyqt5
+pip3.11 install fuzzywuzzy
+pip3.11 install ujson
+pip3.11 install sounddevice
+pip3.11 install vosk
+pip3.11 install -e git+https://github.com/Yuego/num2t4ru#egg=num2t4ru
